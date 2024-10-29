cd081844586f6e262c45ca47003d947abc874f73
FanmKaribeyenn <vnovembre971@gmail.com>
2024-10-29 08:16:53 -0400

    fix carousel

--- src/app/components/Sections/Carousel.tsx
+++ src/app/components/Sections/Carousel.tsx
@@ -46,30 +46,19 @@ const Carousel = () => {
               backgroundSize: 'cover',
               backgroundPosition: 'center',
             }}>
-            {carouselDatas.map((item, index) => (
-              <div
-                key={index}
-                className='w-full h-screen flex-shrink-0'
-                style={{
-                  backgroundImage: `url(${item.url})`,
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'center',
-                }}>
-                <section className=' pt-24 h-screen flex'>
-                  <div className='grid max-w-screen-xl px-4 py-8  mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12'>
-                    <div className='mr-auto place-self-center lg:col-span-7'>
-                      <h1 className='max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white'>
-                        {item.title}
-                      </h1>
-                      <p className='max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400'>
-                        {item.description}
-                      </p>
-                      <Button text={item.btn} link={item.url} />
-                    </div>
-                  </div>
-                </section>
+            <section className=' pt-24 h-screen flex'>
+              <div className='grid max-w-screen-xl px-4 py-8  mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12'>
+                <div className='mr-auto place-self-center lg:col-span-7'>
+                  <h1 className='max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white'>
+                    {item.title}
+                  </h1>
+                  <p className='max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400'>
+                    {item.description}
+                  </p>
+                  <Button text={item.btn} link={item.url} />
+                </div>
               </div>
-            ))}
+            </section>
           </div>
         ))}
       </div>
